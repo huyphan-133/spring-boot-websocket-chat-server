a91b9930a71c5503ac911ba6624ad677222595a8
Huy Phan Viet (IT) <huypv@kbsec.com.vn>
2023-05-26 16:51:21 +0700

    initial project

---
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,33 @@
+package com.example.websocketdemo.controller;
+
+import com.example.websocketdemo.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ChatController {
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        logger.info("sendMessage");
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
